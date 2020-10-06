cda02f6e668b14eefe67f9693ad3bd5898b1a704
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:28:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,12 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costumes_stores do |t|
       t.string   :name
       t.string   :location
-      t.integer   :theme
-      t.float   :price
+      t.integer   :costume_inventory
+      t.integer   :number_of_employees
       t.string  :
       t.date    :opening_date
       t.date    :closing_date
-      t.string  :long_description
       t.timestamps
      end
   end
