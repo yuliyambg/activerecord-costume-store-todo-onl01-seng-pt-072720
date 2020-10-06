c055dab2ab24b29de4ba93b0d5494c256d048a20
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:32:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer   :costume_inventory
       t.integer   :num_of_employees
       t.boolean  :still_in_business
-      t.date    :opening_date
+      t.datetime    :opening_time
       t.date    :closing_date
       t.timestamps
      end
