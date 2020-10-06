7c3782347f6f2d69e238c3876738713250572127
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:47:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,12 +4,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string   :name
       t.string   :location
-      t.string  :theme
-      t.float  :price
+      t.string   :theme
+      t.float    :price
       t.boolean  :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text   :description
+      t.text     :description
      end
   end
   
