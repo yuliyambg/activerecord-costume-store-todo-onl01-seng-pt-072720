44f6ca1d528d6da3c45cffd241540fb66eb2ee8e
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:39:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes_stores do |t|
+    create_table :costume_stores do |t|
       t.string   :name
       t.string   :location
       t.integer  :costume_inventory
