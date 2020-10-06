411328acbcd6ebe5929ec333970596053574b8af
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:12:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string   :name
-      t.integer  :price
+      t.float    :price
       t.string   :size
       t.url      :image
     end
