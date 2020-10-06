fef2b7bcd34d8a21ed938f79bf6ae0575fa5576a
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:26:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,18 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes_stores do |t|
+      t.string   :name
+      t.string   :location
+      t.string   :theme
+      t.float   :price
+      t.string  :
+      t.date    :opening_date
+      t.date    :closing_date
+      t.string  :long_description
+      t.timestamps
+     end
+  end
+  
+end
