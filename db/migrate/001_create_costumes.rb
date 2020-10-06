24c8b7f23012b943ce221454768f88516db684af
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:08:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,6 +8,6 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[5.1]
   
 end
