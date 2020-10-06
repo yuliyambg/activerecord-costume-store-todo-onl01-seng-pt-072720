f0adf2685ff77a4cc7c3a0abe8da72dd40a3915b
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 03:47:04 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean  :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string   :long_long_description
+      t.text   :description
      end
   end
   
